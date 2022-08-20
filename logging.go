9dab7a7864cccf37b4088a4723927a6e10e1bad4
seud0nym <seud0nym@yahoo.com.au>
2022-08-20 12:02:05 +1000

    Use printf correctly

--- logging.go
+++ logging.go
@@ -13,7 +13,7 @@ func logging(console bool) {
 	logger, err := syslog.New(syslog.LOG_DAEMON|syslog.LOG_NOTICE, "tproxy-go")
 	if err != nil {
 		log.SetOutput(os.Stderr)
-		log.Printf("ERROR: Writing to stderr because failed to open syslog (", err, ")")
+		log.Printf("ERROR: Writing to stderr because failed to open syslog (%s)", err)
 	} else {
 		log.SetFlags(0)
 		if console {
